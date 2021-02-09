448746c5e5ec0157c104852346d61254e3275f1b
Senthil Pitchappan V <66490366+v-senthil@users.noreply.github.com>
2021-02-09 23:32:36 +0530

    Update env.py

--- env.py
+++ env.py
@@ -1,7 +1,7 @@
-TWILIO_ACCOUNT_SID = "ACacb90c566a2a5372da882c193931b079"
-TWILIO_AUTH_TOKEN = "f1d835009224d5ab1254204d6329c2cd"
-TWILIO_CALLER_ID = '+16123602566'
-GMAIL_EMAIL = "senthilvalliappan99@gmail.com"
-GMAIL_PASSWORD = "301403@senthilvalliappanV"
+TWILIO_ACCOUNT_SID = " "
+TWILIO_AUTH_TOKEN = " "
+TWILIO_CALLER_ID = ' '
+GMAIL_EMAIL = "GMAIL_ID"
+GMAIL_PASSWORD = "GMAIL_PSD"
 API_KEY = "senthilV"
-MONGO_DB_URI = "mongodb+srv://senthil:senthilV@python.7iyoc.mongodb.net/Python?retryWrites=true&w=majority"
+MONGO_DB_URI = "MONGO_DB_STRING"
